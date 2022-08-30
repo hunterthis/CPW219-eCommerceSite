2a7f6f9fdc2045d80019d47fb8415dd1a844b7da
Hunter Larsen <larsen7605@students.cptc.edu>
2022-08-30 15:32:59 -0700

    commented cookie function

--- Models/Trees.cs
+++ Models/Trees.cs
@@ -9,11 +9,15 @@ namespace CPW219_eCommerceSite.Models
         public string Name { get; set; }
         public string Description { get; set; }
     }
+
+
+    /// <summary>
+    /// Creates cookies model for trees to enter into webpage and buy stuff
+    /// </summary>
     public class CartTreeViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public string Description { get; set; }
     }
 

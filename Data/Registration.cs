78498890fbbb2649b4b0b9a3b2a4d93caa9413c6
Hunter Larsen <larsen7605@students.cptc.edu>
2022-08-30 11:18:00 -0700

    added registration and shopper class

--- Data/Registration.cs
+++ Data/Registration.cs
@@ -5,6 +5,11 @@ namespace CPW219_eCommerceSite.Data
 {
     public class Registration :DbContext
     {
+        public Registration(DbContextOptions<Registration> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Shopper> Shoppers { get; set; }
         
     }
 }

77cde777a7165cee369caf6b4846724283d642f5
Hunter Larsen <larsen7605@students.cptc.edu>
2022-08-30 15:23:00 -0700

    implemented cart controller partially

---
+++ Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly UserContext _context;
+
+        public CartController(UserContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Trees treeToAdd = _context.Trees.Where(t => t.Id == id).SingleOrDefault();
+
+            if(treeToAdd == null)
+            {
+                return RedirectToAction("Index","Trees");
+            }
+            return View();
+        }
+    }
+}

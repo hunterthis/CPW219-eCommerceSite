1f4936d8586992bc242edbb28e6ad954fa9ae8e3
Hunter Larsen <larsen7605@students.cptc.edu>
2022-08-30 11:40:28 -0700

    added user database information for storing

---
+++ Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_CRUD_Troubleshooting.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/Userdb.cs
@@ -0,0 +1,41 @@
+﻿namespace CPW219_eCommerceSite.Models
+{
+    public static class UserDb
+    {
+        public static User Add(User p, UserContext db)
+        {
+            //Add User to context
+            db.Users.Add(p);
+            return p;
+        }
+
+        public static List<User> GetUsers(UserContext context)
+        {
+            return (from s in context.Users
+                    select s).ToList(); // need to fix, cannot access database
+        }
+
+        public static User GetUser(UserContext context, int id)
+        {
+            User p2 = context
+                            .Users
+                            .Where(s => s.UserId == id)
+                            .Single();
+            return p2;
+        }
+
+        public static void Delete(UserContext context, User p)
+        {
+            context.Users.Update(p);
+        }
+
+        public static void Update(UserContext context, User p)
+        {
+            //Mark the object as deleted
+            context.Users.Remove(p);
+
+            //Send delete query to database
+            context.SaveChanges();
+        }
+    }
+}

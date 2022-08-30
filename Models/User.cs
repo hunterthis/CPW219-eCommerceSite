1d73abb1054871a3b9fa2fcc8d31eadc64d232da
Hunter Larsen <larsen7605@students.cptc.edu>
2022-08-30 11:35:51 -0700

    added User class for storing info

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CPW219_eCommerceSite.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string? Name { get; set; }
+        public string? password { get; set; }
+
+    }
+}
